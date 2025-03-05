2de407c1e54074c24953cd51e058f7d54ac267ba
default <devin.boucher@stu.ocsb.ca>
2025-03-05 18:05:21 +0000

    coyed js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Feb 2025
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 function myButtonClicked () {
