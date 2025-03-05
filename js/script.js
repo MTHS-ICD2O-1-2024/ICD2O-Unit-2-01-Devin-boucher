446302faf4c5f5a7e3894ab381743ea42ffe711f
default <devin.boucher@stu.ocsb.ca>
2025-03-05 18:12:00 +0000

    added comment to js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // Created on: Mar 2025
 // This file contains the JS functions for index.html
 
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
 function myButtonClicked () {
   document.getElementById("answer").innerHTML = "Hello, World!"
 }
